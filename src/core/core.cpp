914e56fd42583490937e78e81953ec38742a75f7
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-09-08 20:22:42 -0400

    sdk_message wrapper function

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,8 +1,10 @@
 #include "core.h"
 
 #include <Windows.h>
+#include <winuser.h>
 
 #include "src/sdk/interfaces/cvar.h"
+#include "src/sdk/interfaces/game_console.h"
 
 bool core::check_insecure() {
   int     argc;
@@ -27,22 +29,25 @@ bool core::check_insecure() {
 }
 
 bool core::initialize() {
+  float time = 0.f;
+  while (!GetModuleHandleA("ServerBrowser.dll")) {
+    Sleep(500), time += 0.5f;
+    if (time >= 60.f || (GetAsyncKeyState(VK_DELETE) & 1))
+      return false;
+  }
+
   if (!g_interfaces.collect_interfaces())
     return false;
 
-  g_interfaces.cvar->console_color_printf(color_t{255, 255, 255, 255}, "SDK | ");
-  g_interfaces.cvar->console_color_printf(color_t{250, 202, 222, 255},
-                                          "Interfaces initialized\n");
+  g_interfaces.game_console->activate();
+  g_interfaces.game_console->clear();
+  sdk_message(color_t{250, 202, 222, 255}, "Interfaces initialized");
 
   if (!g_addresses.collect_addresses())
     return false;
+  sdk_message(color_t{250, 202, 222, 255}, "Addresses initialized");
 
-  g_interfaces.cvar->console_color_printf(color_t{255, 255, 255, 255}, "SDK | ");
-  g_interfaces.cvar->console_color_printf(color_t{250, 202, 222, 255},
-                                          "Addresses initialized\n");
-
-  g_interfaces.cvar->console_color_printf(color_t{255, 255, 255, 255}, "SDK | ");
-  g_interfaces.cvar->console_color_printf(color_t{0, 255, 0, 255}, "Loaded\n");
+  sdk_message(color_t{0, 255, 0, 255}, "Loaded");
 
   g_globals.attached = true;
   return true;
@@ -51,7 +56,20 @@ bool core::initialize() {
 bool core::should_unload() { return (GetAsyncKeyState(VK_DELETE) & 1); }
 
 void core::unload() {
-  g_interfaces.cvar->console_color_printf(color_t{255, 255, 255, 255}, "SDK | ");
-  g_interfaces.cvar->console_color_printf(color_t{255, 0, 0, 255}, "Unloaded\n");
   g_globals.unloading = true;
+  g_interfaces.game_console->activate();
+  g_interfaces.game_console->clear();
+  sdk_message(color_t{255, 0, 0, 255}, "Unloaded");
+}
+
+void core::sdk_message(const color_t& color, const char* format, ...) {
+  g_interfaces.cvar->console_color_printf(color_t{255, 255, 255, 255}, "SDK | ");
+
+  char    buffer[1024];
+  va_list args;
+  va_start(args, format);
+  vsnprintf(buffer, sizeof(buffer), format, args);
+  va_end(args);
+  g_interfaces.cvar->console_color_printf(color, buffer);
+  g_interfaces.cvar->console_printf("\n");
 }
--- src/core/core.h
+++ src/core/core.h
@@ -4,6 +4,8 @@
 #include "interfaces/interfaces.h"
 #include "addresses/addresses.h"
 
+struct color_t;
+
 namespace core {
 
   inline globals_t    g_globals;
@@ -14,4 +16,6 @@ namespace core {
   bool initialize();
   bool should_unload();
   void unload();
+
+  void sdk_message(const color_t& color, const char* message, ...);
 } // namespace core
